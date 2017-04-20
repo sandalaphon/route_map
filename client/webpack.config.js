43ff9c7a5b74a6a18a10ef8b71e4224698e524e1
Richard Page-Croft <rpcroft@gmail.com>
2017-04-20 16:04:26 +0100

    white-space conflict resolved

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
   config = {
     entry: "./src/app.js",
 
@@ -11,15 +11,4 @@
   }
 
   module.exports = config;
-=======
-config = {
-  entry: "./src/app.js",
-  output: {
-    filename: "bundle.js",
-    path: "./build"
-  },
-  devtool: 'source-map'
-}
 
-module.exports = config;
->>>>>>> 82511100df486e08ed0880eb5d9e39d1d64ae833
