03748ac3ee11c2fce8ea0e674bec24420ff07b9c
Richard Page-Croft <rpcroft@gmail.com>
2017-04-21 19:35:24 +0100

    journey distance and time calculated and displayed, draggable start stop

--- client/src/mapWrapper.js
+++ client/src/mapWrapper.js
@@ -16,7 +16,7 @@ MapWrapper.prototype ={
   },
 
   addClickEvent: function(){
-    google.maps.event.addListener(this.googleMap, 'click', function(event){
+    google.maps.event.addListener(this.googleMap, 'dblclick', function(event){
       var latitude = event.latLng.lat()
       var longitude = event.latLng.lng() 
       localStorage.setItem("latitude", latitude)
@@ -28,15 +28,49 @@ MapWrapper.prototype ={
 
   drawRoute:function(directionsResult){
     var directionsService = new google.maps.DirectionsService;
-    var directionsDisplay = new google.maps.DirectionsRenderer;
-    directionsDisplay.setMap(this.googleMap);
+    var directionsDisplay = new google.maps.DirectionsRenderer({
+      draggable: true,
+      map: this.googleMap
+    });
+    // directionsDisplay.setMap(this.googleMap);
 
     directionsService.route(directionsResult, function(res, status){
       if(status== 'OK'){
         directionsDisplay.setDirections(res)
+        this.computeTotalDistance(directionsDisplay.getDirections());
+        this.computeEstimatedTime(directionsDisplay.getDirections());
+        directionsDisplay.addListener('directions_changed', function() {
+          // directionsDisplay.setDirections(res)
+                  this.computeTotalDistance(directionsDisplay.getDirections());
+
+                });
       }
-    })
-  }
+    }.bind(this)) 
+  },
+  // compute total distance and display
+   computeTotalDistance: function(result) {
+          var total = 0;
+          var myroute = result.routes[0];
+          for (var i = 0; i < myroute.legs.length; i++) {
+            total += myroute.legs[i].distance.value;
+          }
+          total = total / 1000;
+          document.getElementById('total').innerHTML = total + ' km';
+        },
+
+    computeEstimatedTime: function(result) {
+           var totalSeconds = 0;
+           var myroute = result.routes[0];
+           for (var i = 0; i < myroute.legs.length; i++) {
+             totalSeconds += myroute.legs[i].duration.value;
+           }
+           var remainderSeconds = totalSeconds%60
+           var totalMinutes = (totalSeconds-remainderSeconds)/60
+           var remainderMinutes = totalMinutes%60
+           var hours = (totalMinutes-remainderMinutes)/60
+
+           document.getElementById('time').innerHTML = hours + ' hours '+ remainderMinutes +' minutes and ' + remainderSeconds + ' seconds';
+         }
 }
 
 module.exports = MapWrapper;
