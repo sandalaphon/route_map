a64ba7117b6b47822cd746cbe565946b1cd89893
Ewen Carr <ewencarr@hotmail.com>
2017-04-21 10:54:53 +0100

    index route hitting

--- controllers/route_planner_router.js
+++ controllers/route_planner_router.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var routePlannerRouter = express.Router();
+var RoutePlannerQuery = require('../db/route_planner_query.js');
+var routePlannerQuery = new RoutePlannerQuery();
 
+
+//INDEX
 routePlannerRouter.get('/', function(req, res){
-  res.json({ data: [{name: "Earth"}]});
+  routePlannerQuery.all(function(docs){
+    res.json(docs);
+  })
+ 
 
 })
+
+//SHOW
+
+
+//CREATE
+routePlannerRouter.post('/', function(req, res){
+  routePlannerQuery.add(route)
+})
+
+
+//DELETE
+
+
+
+//UPDATE
+
 
 module.exports = routePlannerRouter;
