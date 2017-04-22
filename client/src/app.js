72baa793fe4a539da7f9161955fc1056f8592d85
Douglas Crooke <douglas.crooke+codeclan@googlemail.com>
2017-04-22 21:50:58 +0100

    cleaned out commented out code

--- client/src/app.js
+++ client/src/app.js
@@ -9,51 +9,6 @@ var app = function () {
 
   page.setupButtons()
 
-  // var startButton = document.querySelector("#start");
-  // var endButton = document.querySelector('#finish');
-  // var routeButton = document.querySelector('#route');
-  // var cyclingButton = document.querySelector('#cycling');
-  // var walkingButton = document.querySelector('#walking');
-  // var saveButton = document.querySelector('#save');
-
-  // var center = {lat: 55.953251, lng: -3.188267}
-  // var containerDiv = document.querySelector('#main-map')
-  // var mainMap = new MapWrapper(containerDiv, center, 5)
-  // var transportMethod
-  // var routeName
-  // var route = null;
-
-  // startButton.addEventListener('click', function(){
-  //   mainMap.addStartClickEvent();
-  //   })
-
-  // endButton.addEventListener('click', function () {
-  //   mainMap.addFinishClickEvent()
-  // })
-
-  // cyclingButton.addEventListener('click', function () {
-  //   transportMethod = 'BICYCLING'
-  // })
-
-  // walkingButton.addEventListener('click', function () {
-  //   transportMethod = 'WALKING'
-  // })
-
-  // routeButton.addEventListener('click',
-    // function () {
-    //   var startLatitude = localStorage.getItem('startLatitude') // need better names for storage
-    //   var startLongitude = localStorage.getItem('startLongitude') // same here
-    //   var finishLatitude = localStorage.getItem('finishLatitude')
-    //   var finishLongitude = localStorage.getItem(
-    //     'finishLongitude')
-    //   var start = {lat: +startLatitude, lng: +startLongitude}
-    //   var end = {lat: +finishLatitude, lng: +finishLongitude}
-    //   console.log(start, end)
-    //   var directions = new Route(start, end, transportMethod)
-    //   route = directions.directions()
-    //   mainMap.drawRoute(route)
-    // })
-
   saveButton.addEventListener('click', function () {
     if (route) {
       var jsonString = JSON.stringify(route)
