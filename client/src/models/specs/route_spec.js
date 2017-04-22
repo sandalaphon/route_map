e5c73988530c40077e23069625ddd4efca500ea9
Douglas Crooke <douglas.crooke+codeclan@googlemail.com>
2017-04-22 15:31:35 +0100

    corrections to test command and deepEquals instead of strictEquals

--- client/src/models/specs/route_spec.js
+++ client/src/models/specs/route_spec.js
@@ -1,29 +1,26 @@
-var Route = require('../route.js');
-var assert = require('assert');
+var Route = require('../route.js')
+var assert = require('assert')
 
-describe("Route", function(){
+describe('Route', function () {
+  var route1
 
-  var route1;
-
-  beforeEach(function(){
-    route1 = new Route({lat:59.5, lng:-3.2},{lat:51.5, lng:0.1}, "CYCLING", "Edinburgh to London");
-  });
-
-  it("Route has name", function(){
-    assert.strictEqual("Edinburgh to London", route1.name);
+  beforeEach(function () {
+    route1 = new Route({lat: 59.5, lng: -3.2}, {lat: 51.5, lng: 0.1}, 'CYCLING', 'Edinburgh to London')
   })
 
-  it("Route starts in Edinburgh", function(){
-    assert.strictEqual({lat:59.5, lng:-3.2}, route1.origin);
-  }
-
-  it("Route ends in London", function(){
-    assert.strictEqual({lat:51.5, lng:0.1}, route1.destination);
+  it('Route has name', function () {
+    assert.strictEqual('Edinburgh to London', route1.name)
   })
 
-  it("Route has travel method", function(){
-    assert.strictEqual("CYCLING", route1.travelMode);
+  it('Route starts in Edinburgh', function () {
+    assert.deepEqual({lat: 59.5, lng: -3.2}, route1.origin)
   })
 
+  it('Route ends in London', function () {
+    assert.deepEqual({ lat: 51.5, lng: 0.1 }, route1.destination)
+  })
 
+  it('Route has travel method', function () {
+    assert.strictEqual('CYCLING', route1.travelMode)
+  })
 })
