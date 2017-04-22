ef076ea1c601fdc9198c396802967122117fb93b
Ewen Carr <ewencarr@hotmail.com>
2017-04-22 15:13:39 +0100

    testing models

---
+++ client/src/models/specs/route_spec.js
@@ -0,0 +1,29 @@
+var Route = require('../route.js');
+var assert = require('assert');
+
+describe("Route", function(){
+
+  var route1;
+
+  beforeEach(function(){
+    route1 = new Route({lat:59.5, lng:-3.2},{lat:51.5, lng:0.1}, "CYCLING", "Edinburgh to London");
+  });
+
+  it("Route has name", function(){
+    assert.strictEqual("Edinburgh to London", route1.name);
+  })
+
+  it("Route starts in Edinburgh", function(){
+    assert.strictEqual({lat:59.5, lng:-3.2}, route1.origin);
+  }
+
+  it("Route ends in London", function(){
+    assert.strictEqual({lat:51.5, lng:0.1}, route1.destination);
+  })
+
+  it("Route has travel method", function(){
+    assert.strictEqual("CYCLING", route1.travelMode);
+  })
+
+
+})
