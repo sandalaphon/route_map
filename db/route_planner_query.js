59a253a213b917b0b8232a25fbf609fe0fc4a91d
Douglas Crooke <douglas.crooke+codeclan@googlemail.com>
2017-04-22 15:01:48 +0100

    re-styled code layout for backend

--- db/route_planner_query.js
+++ db/route_planner_query.js
@@ -1,65 +1,65 @@
-var MongoClient = require('mongodb').MongoClient;
-var ObjectID = require('mongodb').ObjectID;
+var MongoClient = require('mongodb').MongoClient
+var ObjectID = require('mongodb').ObjectID
 
-var RoutesQuery = function(collectionName){
-  this.url = "mongodb://localhost:27017/route_planner";
+var RoutesQuery = function (collectionName) {
+  this.url = 'mongodb://localhost:27017/route_planner'
   this.collection = collectionName
-};
+}
 
 RoutesQuery.prototype = {
-  all: function(onQueryFinished){
-    MongoClient.connect(this.url,function(err, db){
-      if(db){
-        var collection = db.collection(this.collection);
-        collection.find().toArray(function(err, docs){
-          onQueryFinished(docs);
-        })
-      }
-    }.bind(this))
-  },
-  add: function(routeToAdd, onQueryFinished){
-    MongoClient.connect(this.url, function(err, db){
-      if(db){
-        var collection = db.collection(this.collection);
-        collection.insert(routeToAdd);
-        collection.find().toArray(function(err,docs){
-          onQueryFinished(docs);
-        })
-      }
-    }.bind(this))
-  },
-  find: function(routeID, onQueryFinished){
-    MongoClient.connect(this.url, function(err, db){
-      if(db){
-        var collection = db.collection(this.collection);
-        collection.find({_id: ObjectID(routeID)}).toArray(function(err, docs){
+  all: function (onQueryFinished) {
+    MongoClient.connect(this.url, function (err, db) {
+      if (db) {
+        var collection = db.collection(this.collection)
+        collection.find().toArray(function (err, docs) {
           onQueryFinished(docs)
         })
       }
-    }.bind(this))  
+    }.bind(this))
   },
-  delete: function(routeID, onQueryFinished){
-    MongoClient.connect(this.url, function(err, db){
-      if(db){
-        var collection = db.collection(this.collection);
-        collection.remove({_id: ObjectID(routeID)});
-        collection.find().toArray(function(err, docs){
-          onQueryFinished(docs);
+  add: function (routeToAdd, onQueryFinished) {
+    MongoClient.connect(this.url, function (err, db) {
+      if (db) {
+        var collection = db.collection(this.collection)
+        collection.insert(routeToAdd)
+        collection.find().toArray(function (err, docs) {
+          onQueryFinished(docs)
         })
       }
     }.bind(this))
   },
-  update: function(routeID, payload, onQueryFinished){
-    MongoClient.connect(this.url, function(err, db){
-      if(db){
-        var collection = db.collection(this.collection);
-        collection.updateOne({_id: ObjectID(routeID)}, {$set: payload}, function(err, docs){
-          onQueryFinished(docs);
-        });
+  find: function (routeID, onQueryFinished) {
+    MongoClient.connect(this.url, function (err, db) {
+      if (db) {
+        var collection = db.collection(this.collection)
+        collection.find({_id: ObjectID(routeID)}).toArray(function (err, docs) {
+          onQueryFinished(docs)
+        })
       }
     }.bind(this))
-  }  
+  },
+  delete: function (routeID, onQueryFinished) {
+    MongoClient.connect(this.url, function (err, db) {
+      if (db) {
+        var collection = db.collection(this.collection)
+        collection.remove({_id: ObjectID(routeID)})
+        collection.find().toArray(function (err, docs) {
+          onQueryFinished(docs)
+        })
+      }
+    }.bind(this))
+  },
+  update: function (routeID, payload, onQueryFinished) {
+    MongoClient.connect(this.url, function (err, db) {
+      if (db) {
+        var collection = db.collection(this.collection)
+        collection.updateOne({_id: ObjectID(routeID)}, {$set: payload}, function (err, docs) {
+          onQueryFinished(docs)
+        })
+      }
+    }.bind(this))
+  }
 
 }
 
-module.exports = RoutesQuery;
+module.exports = RoutesQuery
--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
+var express = require('express')
+var app = express()
+var bodyParser = require('body-parser')
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
-app.use(require('./controllers/index.js'));
+app.use(require('./controllers/index.js'))
 
-app.use(express.static('client/build'));
+app.use(express.static('client/build'))
 
-app.listen(3000, function(){
-  console.log("listening on port 3000")
+app.listen(3000, function () {
+  console.log('listening on port 3000')
 })
