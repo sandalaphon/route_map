6060805e25b238ab3bf79a366e338f90608bb89d
Douglas Crooke <douglas.crooke+codeclan@googlemail.com>
2017-04-24 10:45:54 +0100

    corrections to tests to keep up with other changes

--- client/src/models/route.js
+++ client/src/models/route.js
@@ -2,25 +2,25 @@ var MakeRequest = require('./make_requests.js')
 // GOOGLE MAPS OBJECT FACTORY!
 
 var Route = function (origin, destination, travelMode) {
-  this.name = null,
+  this.name = null
   // this.author = author
-  this.rating = null,
-  this.origin = origin,
-  this.destination = destination,
-  this.waypoints = [],
-  this.travelMode = travelMode,
+  this.rating = null
+  this.origin = origin
+  this.destination = destination
+  this.waypoints = []
+  this.travelMode = travelMode
   this.calculatedRoute = {}     // the directions calculated by whatever routing engine is used (google directions)  // think we can lose this
-  this.googleResponse = null,
-  this.optimizeWaypoints = true// this.provideRouteAlternatives = true;
+  this.googleResponse = null
+  this.optimizeWaypoints = true  // this.provideRouteAlternatives = true;
 }
 
 Route.prototype = {
 
-  addName: function(name){
+  addName: function (name) {
     this.name = name
   },
 
-  addGoogleResponse: function(directionServiceResponse){
+  addGoogleResponse: function (directionServiceResponse) {
     this.googleResponse = directionServiceResponse
   },
 
