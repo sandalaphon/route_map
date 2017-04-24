cb2e75c5146828c5d82adf875b3d9231c929ceaf
Douglas Crooke <douglas.crooke+codeclan@googlemail.com>
2017-04-24 21:30:52 +0100

    comment out unnecessary requires

--- client/src/app.js
+++ client/src/app.js
@@ -1,13 +1,13 @@
-var MapWrapper = require('./mapWrapper.js')
-var Route = require('./models/route.js')
+// var MapWrapper = require('./mapWrapper.js')
+// var Route = require('./models/route.js')
 // var mainMap
 var Page = require('./views/setup_page.js')
 var Sidebar = require('./views/sidebar.js')
-var SuggestionList = require('./views/suggested_list.js')
-var MakeRequest = require('./models/make_requests.js')
+// var SuggestionList = require('./views/suggested_list.js')
+// var MakeRequest = require('./models/make_requests.js')
 
 var app = function () {
-  localStorage.clear()
+  // localStorage.clear()
 
   var page = new Page()
   var sidebar = new Sidebar(page)
@@ -15,9 +15,8 @@ var app = function () {
 
   // SIDEBAR
 
-  console.log(sidebar)
+  // console.log(sidebar)
   page.setupSideBars(sidebar)
-
 }
 
 window.onload = app
