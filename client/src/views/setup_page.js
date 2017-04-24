944ccbab0436028106f3e8af16d200656d06293c
Douglas Crooke <douglas.crooke+codeclan@googlemail.com>
2017-04-24 21:41:40 +0100

    tidy up formatting/indentation

--- client/src/views/setup_page.js
+++ client/src/views/setup_page.js
@@ -4,7 +4,7 @@ var MakeRequest = require('../models/make_requests.js')
 var SuggestionList = require('./suggested_list.js')
 var Forecast = require('../models/forecast.js')
 var WeatherView = require('../models/weather_view')
-var weatherView = new WeatherView
+var weatherView = new WeatherView()
 
 var Page = function () {
   this.page = document
@@ -28,38 +28,29 @@ var Page = function () {
     mainMap: new MapWrapper(containerDiv, {lat: 55.953251, lng: -3.188267}, 5),
     transportMethod: 'BICYCLING'
   }
-
-  // sessionStorage.setItem('saved-map', this.map.mainMap);
-  // sessionStorage.setItem('saved-map2', "hello");
-
-  // console.log(this.map.mainMap)
 }
 
 Page.prototype = {
 
   setupSideBars: function (sidebar) {
-    var makeRequest = new MakeRequest();
-      // var sidebar = new Sidebar(this)
-      console.log(sidebar)
+    var makeRequest = new MakeRequest()
 
-      sidebar.populateList(makeRequest.makeGetRequest)
-      console.log(' before populate, ' , sidebar.sidebarHidden)
-      if (sidebar.sidebarHidden) {
-        sidebar.sidebarHTMLObject.style.display = 'none'
-      }
-      else {
-        sidebar.sidebarHTMLObject.style.diplay = 'inline-block'
-      }
+    sidebar.populateList(makeRequest.makeGetRequest)
+    if (sidebar.sidebarHidden) {
+      sidebar.sidebarHTMLObject.style.display = 'none'
+    } else {
+      sidebar.sidebarHTMLObject.style.diplay = 'inline-block'
+    }
 
-      var suggestionList = new SuggestionList(this)
-      suggestionList.populateList(makeRequest.makeGetRequest)
-      suggestionList.sidebarHTMLObject.style.display = 'none'
+    var suggestionList = new SuggestionList(this)
+    suggestionList.populateList(makeRequest.makeGetRequest)
+    suggestionList.sidebarHTMLObject.style.display = 'none'
 
-      var wishlistRevealButton = document.querySelector('#wishlist-button')
-      wishlistRevealButton.addEventListener('click', sidebar.revealWishlist)
+    var wishlistRevealButton = document.querySelector('#wishlist-button')
+    wishlistRevealButton.addEventListener('click', sidebar.revealWishlist)
 
-      var suggestionListRevealButton = document.querySelector('#suggested')
-      suggestionListRevealButton.addEventListener('click', suggestionList.revealList)
+    var suggestionListRevealButton = document.querySelector('#suggested')
+    suggestionListRevealButton.addEventListener('click', suggestionList.revealList)
   },
 
   setupButtons: function (sidebar) {
@@ -72,56 +63,53 @@ Page.prototype = {
     this.setButtonEvent('click', this.buttons['walking'], function () {
       this.map.transportMethod = 'WALKING'
     }.bind(this))
-    this.setButtonEvent('click', this.buttons['findAmenity'], function(){
+    this.setButtonEvent('click', this.buttons['findAmenity'], function () {
       var finLat = localStorage.getItem('finishLatitude')
       var finLng = localStorage.getItem('finishLongitude')
       var coords = {lat: +finLat, lng: +finLng}
-      this.map.mainMap.googleMap.setZoom(10),
+      this.map.mainMap.googleMap.setZoom(10)
       this.map.mainMap.googleMap.setCenter(coords)
-      var radius = 10000 //change cycling or walking
-      this.map.mainMap.placesService(coords, radius, "restaurant")
+      var radius = 10000 // change cycling or walking
+      this.map.mainMap.placesService(coords, radius, 'restaurant')
     }.bind(this))
     this.setButtonEvent('click', this.buttons['route'], this.map.mainMap.calculateRoute.bind(this.map))
 
-    //forecast function
-    this.setButtonEvent('click', this.buttons['forecast'], function(){
-    var latitude = localStorage.getItem("finishLatitude");
-    var longitude = localStorage.getItem("finishLongitude");
-    var url = "http://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&APPID=f66376ebcb0af19199eb5e28d449aaf9";
-    var forecast = new Forecast(url).getData(function(weather){
-      weatherView.render(weather);
-      console.log(weather)
-    })
+    // forecast function
+    this.setButtonEvent('click', this.buttons['forecast'], function () {
+      var latitude = localStorage.getItem('finishLatitude')
+      var longitude = localStorage.getItem('finishLongitude')
+      var url = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&APPID=f66376ebcb0af19199eb5e28d449aaf9'
+      var forecast = new Forecast(url).getData(function (weather) {
+        weatherView.render(weather)
+        console.log(weather)
+      })
+    }) // forecastfunction)
 
-  }) //forecastfunction)
-
-    this.setButtonEvent('click', this.buttons['save'], function(){
-    var routeName = document.querySelector('#routeName').value 
-    var currentRoute = this.map.mainMap.currentRoute
-    if(!routeName){
-      alert("Please enter a route Name");
-      this.map.mainMap.saveRoute.bind(this.map)
+    this.setButtonEvent('click', this.buttons['save'], function () {
+      var routeName = document.querySelector('#routeName').value
+      var currentRoute = this.map.mainMap.currentRoute
+      if (!routeName) {
+        alert('Please enter a route Name')
+        this.map.mainMap.saveRoute.bind(this.map)
       }
-      var googleResponse = this.map.mainMap.currentRoute.request      //save if route is named and defined
+      var googleResponse = this.map.mainMap.currentRoute.request      // save if route is named and defined
       var originAddressId = this.map.mainMap.currentRoute.geocoded_waypoints[0].place_id
-      var destinationAddressId = this.map.mainMap.currentRoute.geocoded_waypoints[this.map.mainMap.currentRoute.geocoded_waypoints.length-1].place_id
-    
-      this.getAddressFromGeoCode(destinationAddressId, function(streetName){
-        var destinationAddress = streetName;
+      var destinationAddressId = this.map.mainMap.currentRoute.geocoded_waypoints[this.map.mainMap.currentRoute.geocoded_waypoints.length - 1].place_id
 
-        this.getAddressFromGeoCode(originAddressId, function(streetName){
+      this.getAddressFromGeoCode(destinationAddressId, function (streetName) {
+        var destinationAddress = streetName
 
-          var routeToSave = new Route(streetName, destinationAddress, "not needed")
+        this.getAddressFromGeoCode(originAddressId, function (streetName) {
+          var routeToSave = new Route(streetName, destinationAddress, 'not needed')
           routeToSave.addName(routeName)
           routeToSave.addGoogleResponse(googleResponse)
           console.log(routeToSave)
           routeToSave.save()
           console.log(this)
-          console.log('about to setup sidebars')     
-          this.setupSideBars(this.sidebar) 
-          console.log('just setup sidebars')     
-        }.bind(this));
-
+          console.log('about to setup sidebars')
+          this.setupSideBars(this.sidebar)
+          console.log('just setup sidebars')
+        }.bind(this))
       }.bind(this))
 
       // if( (googleResponse) && (routeName) ){
@@ -132,78 +120,69 @@ Page.prototype = {
       //   console.log(routeToSave)
       //   routeToSave.save()
       // }
-      }.bind(this))
-
-    //test
-    this.setButtonEvent('click', this.buttons['viewsavedRouteButton'], function(){
-    var routeName = document.querySelector('#savedRouteName').value
-    this.viewRoute(routeName)
-  }.bind(this))// we have no idea
-
-      this.setButtonEvent('click', this.buttons['animationButton'], function(){
-      this.map.mainMap.animateRoute()
-      
     }.bind(this))
-    
+
+    // test
+    this.setButtonEvent('click', this.buttons['viewsavedRouteButton'], function () {
+      var routeName = document.querySelector('#savedRouteName').value
+      this.viewRoute(routeName)
+    }.bind(this))// we have no idea
+
+    this.setButtonEvent('click', this.buttons['animationButton'], function () {
+      this.map.mainMap.animateRoute()
+    }.bind(this))
   },
 
-
-      getAddressFromGeoCode: function(addressId, callback){
-          var geoCoder = new google.maps.Geocoder;
-           geoCoder.geocode({'placeId': addressId}, function(results, status){
-            console.log("we got here and this is: ", this)
-           if(status==="OK"){
-           if(results[0]){
+  getAddressFromGeoCode: function (addressId, callback) {
+    var geoCoder = new google.maps.Geocoder()
+    geoCoder.geocode({'placeId': addressId}, function (results, status) {
+      console.log('we got here and this is: ', this)
+      if (status === 'OK') {
+        if (results[0]) {
               // this.getCountryFromGeoCode(results)
-             
-             var streetName =results[0].formatted_address
-             console.log(streetName)
-             console.log("STREET NAME!!", streetName)
-             console.log("results", results)
-           } else {
-               window.alert('No results found');
-             }
-           } else {
-             window.alert('Geocoder failed due to: ' + status + "\nDont click in the sea!");
-         }
-         console.log("streetName",streetName)
-        callback(streetName);
 
-       }.bind(this))
-           
-   },
+          var streetName = results[0].formatted_address
+          console.log(streetName)
+          console.log('STREET NAME!!', streetName)
+          console.log('results', results)
+        } else {
+          window.alert('No results found')
+        }
+      } else {
+        window.alert('Geocoder failed due to: ' + status + '\nDont click in the sea!')
+      }
+      console.log('streetName', streetName)
+      callback(streetName)
+    }.bind(this))
+  },
 
   setButtonEvent: function (type, button, callback) {
     button.addEventListener(type, callback)
     // console.log(type, button, callback)
   },
-  
-  viewRoute: function(routeName){
-    var request = new XMLHttpRequest();
-    var url = "http://localhost:3000/api/routes/"+routeName
-    request.open("GET", url);
-    request.setRequestHeader("Content-Type", "application/json");
-    request.onload = function(){
-      if(request.status!== 200) {
-        alert("Not Found")
-        return}
-        var jsonString = request.responseText;
-      var directionsServiceObj = JSON.parse(jsonString);
-      console.log("FROM DB: ")
+
+  viewRoute: function (routeName) {
+    var request = new XMLHttpRequest()
+    var url = 'http://localhost:3000/api/routes/' + routeName
+    request.open('GET', url)
+    request.setRequestHeader('Content-Type', 'application/json')
+    request.onload = function () {
+      if (request.status !== 200) {
+        alert('Not Found')
+        return
+      }
+      var jsonString = request.responseText
+      var directionsServiceObj = JSON.parse(jsonString)
+      console.log('FROM DB: ')
       console.log(directionsServiceObj[0].googleResponse)
       console.log(this)
       console.log(request)
       this.map.mainMap.drawRoute(directionsServiceObj[0].googleResponse)
     }.bind(this)
-    console.log("get here??", this)
-    request.send();
-
+    console.log('get here??', this)
+    request.send()
   }
-
 
 }
 
-
-
 module.exports = Page
-
