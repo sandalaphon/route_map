13f29f50db02f0153de83c762b65e03e63cd1ae1
Douglas Crooke <douglas.crooke+codeclan@googlemail.com>
2017-04-25 11:53:11 +0100

    refactor wishlist / suggested panel into setup() methods from Page.setup()

--- client/src/app.js
+++ client/src/app.js
@@ -1,6 +1,7 @@
 
 var Page = require('./views/setup_page.js')
 var Sidebar = require('./views/sidebar.js')
+var SuggestionList = require('./views/suggested_list.js')
 
 var app = function () {
   // localStorage.clear()
@@ -8,12 +9,15 @@ var app = function () {
   var page = new Page()
   // Pass page into sidebar to give access to Page objects and methods
   var sidebar = new Sidebar(page)
+  var suggestionList = new SuggestionList(page)
+  sidebar.setup()
+  suggestionList.setup()
 
   // ugly way to give sidebar back to Page object as an attribute after sidebar created
   page.setupButtons(sidebar)
 
   // Setup Wishlist SIDEBAR & Suggested Routes Sidebar
-  page.setupSideBars(sidebar)
+  // page.setupSideBars(sidebar)
 }
 
 window.onload = app
