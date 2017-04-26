2a62b7e35c26085e9315797a8a3fbd06ae69c0ea
Douglas Crooke <douglas.crooke+codeclan@googlemail.com>
2017-04-26 09:55:16 +0100

    add close wishlist red X in top lefthand corner, in case hide wishlist button

--- client/src/views/sidebar.js
+++ client/src/views/sidebar.js
@@ -27,8 +27,17 @@ Sidebar.prototype = {
     }
   },
 
+  addCloseAction: function (htmlElement) {
+    htmlElement.onclick = function () {
+      console.log(this, htmlElement)
+      this.hideReveal()
+    }.bind(this)
+  },
+
   populateList: function (getAllRoutes) {
     var wishlistUL = document.querySelector('#wishlist')
+
+    this.addCloseAction(document.querySelector('#sidebar-close'))  // using span id=sidebar-close
 
     while (wishlistUL.hasChildNodes()) {
       wishlistUL.removeChild(wishlistUL.lastChild)
@@ -40,7 +49,6 @@ Sidebar.prototype = {
       var parsedList = JSON.parse(this.response)
       parsedList.forEach(function (element) {
         var newLi = document.createElement('li')
-
 
         newLi.innerText = 'Name: ' + element.name + ' \n\nStart: ' + element.origin + '\n\nFinish: ' + element.destination
 
@@ -63,10 +71,9 @@ Sidebar.prototype = {
         doneButton.innerText = 'Done'
         doneButton.onclick = function () {
           // newLi.style.textDecoration = 'line-through'
-          if(element.done){
+          if (element.done) {
             element.done = false
-          }
-          else element.done = true
+          } else element.done = true
         }
 
         var deleteRouteFromDB = function (routeID) {
@@ -89,15 +96,12 @@ Sidebar.prototype = {
         displayRoute.id = 'sidebarDisplayRouteButton'
         displayRoute.innerText = 'Display Route'
         displayRoute.addEventListener('click', function () {
-
-
-
           var mainMap = sidebarScope.page.map.mainMap
-          //!BUG! Routes displaying on top of each other, fixed below
+          //! BUG! Routes displaying on top of each other, fixed below
 
-          //FIX ME TOMORROW!
+          // FIX ME TOMORROW!
           var containerDiv = document.querySelector('#main-map')
-          
+
           // mainMap = new MapWrapper(containerDiv, element.googleResponse.destination, 12)
           mainMap.clearRoutes()
           mainMap.drawRoute(element.googleResponse)
